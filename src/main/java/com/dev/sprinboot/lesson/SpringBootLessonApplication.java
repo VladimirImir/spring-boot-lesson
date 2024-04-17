7bbc454a0bdd42ab8a6f75d64af9722b2f271e32
VladimirImir <mimimvvv@gmail.com>
2024-04-17 22:41:56 +0300

    Spring Boot. 3.1 - Lombok.

--- src/main/java/com/dev/sprinboot/lesson/SpringBootLessonApplication.java
+++ src/main/java/com/dev/sprinboot/lesson/SpringBootLessonApplication.java
@@ -1,13 +1,8 @@
 package com.dev.sprinboot.lesson;
 
-import com.dev.sprinboot.conditional.FirstConditional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Profile;
 
-@Conditional(FirstConditional.class)
-@Profile("dev")
 @SpringBootApplication
 public class SpringBootLessonApplication {
 
--- src/main/java/com/dev/sprinboot/lesson/config/ApplicationConfiguration.java
+++ src/main/java/com/dev/sprinboot/lesson/config/ApplicationConfiguration.java
@@ -1,15 +1,15 @@
 package com.dev.sprinboot.lesson.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+@Slf4j
 @Configuration
 public class ApplicationConfiguration {
-
-    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
 
     @PostConstruct
     public void init() {
